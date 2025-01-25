72f910934c03c39c0f0e49f5dec40b33eaf83232
Chatchai Saratakij <CSaratakij@gmail.com>
2025-01-26 01:38:59 +0700

    Add : knockback (system)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,28 +1,71 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Game
 {
+    public enum KnockBackType
+    {
+        Normal,
+        High
+    }
+    
     [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
         private const float ALLOW_PLAYER_ROTATION = 0.1f;
         private const float DESIRED_ROTATION_SPEED = 0.1f;
         
-        [Header("Setting")]
+        [Header("General")]
         [SerializeField] private int playerIndex = 0;
+        
+        [Header("Setting - Normal Movement")]
         [SerializeField] private float moveSpeed = 5.0f;
         [SerializeField] private float gravity = -9.81f;
+        
+        [Header("Setting - Knockback Movement")]
+        [SerializeField] private KnockBackSetting[] knockBackSettings;
         
         [Header("Animation Smoothing")]
         [Range(0, 1f)] [SerializeField] private float horizontalAnimSmoothTime = 0.2f;
         [Range(0, 1f)] [SerializeField] private float verticalAnimTime = 0.2f;
         [Range(0,1f)] [SerializeField] private float startAnimTime = 0.3f;
         [Range(0, 1f)] [SerializeField] private float stopAnimTime = 0.15f;
+
+        [Serializable]
+        public struct CharacterState
+        {
+            public bool isGrounded;
+            public bool isMoveable;
+        }
+
+        [Serializable]
+        public struct KnockBackSetting
+        {
+            public KnockBackType knockBackType;
+            public float knockbackSpeed;
+            public float knockbackSpeedMultipiler;
+            public float knockbackDuration;
+            public Vector3 knockbackDirection;
+        }
         
-        private bool isGrounded;
-        private bool isMovable = true;
+        // Knockback Status
+        private bool IsKnockBack => (Time.time <= knockbackTimer);
+        private float knockbackTimer = 0.0f;
+        private KnockBackType currentKnockBackType = KnockBackType.Normal;
+        private KnockBackSetting currentKnockbackSetting;
+        
+        // Dash Status
+        private bool IsDash => (Time.time <= dashTimer);
+        private bool CanDash => characterState.isMoveable && characterState.isGrounded && !IsKnockBack && !IsImmobilize;
+        private float dashTimer = 0.0f;
+        private float dashCooldownTimer = 0.0f;
+        
+        // Immobilize Status
+        private bool IsImmobilize => false; // TODO
+        
+        private CharacterState characterState;
         private Vector2 gravityVector;
         private Vector3 inputVector;
         private Vector3 desiredMoveDirection;
@@ -32,7 +75,7 @@ namespace Game
         private CharacterController characterController;
         private CharacterSkinControllerLite characterSkinControllerLite;
         private PlayerInput playerInput;
-
+        
         private void Awake()
         {
             characterController = GetComponent<CharacterController>();
@@ -46,12 +89,30 @@ namespace Game
             mainCamera = Camera.main;
             lastNonZeroDesiredMoveDirection = Vector3.forward;
             characterSkinControllerLite.ChangeMaterialSettings(playerIndex);
+            characterState.isMoveable = true;
+            animator.SetTrigger("angry");
         }
 
         private void Update()
         {
             AnimationHandler();
             MoveHandler();
+
+            #if UNITY_EDITOR
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                //characterState.isMoveable = !characterState.isMoveable;
+                Knockback(-Vector3.forward, KnockBackType.Normal);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Knockback(-Vector3.forward, KnockBackType.High);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Dash(Vector3.forward);
+            }
+            #endif
         }
 
         private void LateUpdate()
@@ -69,38 +130,40 @@ namespace Game
             }
         }
 
+        // TODO : for respawn
+        public void ResetCharacterState()
+        {
+            
+        }
+
         private void AnimationHandler()
         {
-            float speed = new Vector2(inputVector.x, inputVector.y).sqrMagnitude;
-            
-            if (speed > ALLOW_PLAYER_ROTATION) {
-                animator.SetFloat ("Blend", speed, startAnimTime, Time.deltaTime);
-            } else if (speed < ALLOW_PLAYER_ROTATION) {
-                animator.SetFloat ("Blend", speed, stopAnimTime, Time.deltaTime);
+            bool shouldBlendAnimationMoveSpeed = (characterState.isMoveable && !IsKnockBack);
+
+            if (shouldBlendAnimationMoveSpeed)
+            {
+                float speed = new Vector2(inputVector.x, inputVector.y).sqrMagnitude;
+                
+                if (speed > ALLOW_PLAYER_ROTATION)
+                {
+                    animator.SetFloat("Blend", speed, startAnimTime, Time.deltaTime);
+                }
+                else if (speed < ALLOW_PLAYER_ROTATION)
+                {
+                    animator.SetFloat("Blend", speed, stopAnimTime, Time.deltaTime);
+                }
+            }
+            else
+            {
+                animator.SetFloat("Blend", 0.0f, 0.0f, Time.deltaTime);
             }
         }
         
         private void MoveHandler()
         {
-            isGrounded = characterController.isGrounded;
+            characterState.isGrounded = characterController.isGrounded;
 
-            Vector3 forwardBasis = mainCamera.transform.forward;
-            Vector3 rightBasis = mainCamera.transform.right;
-
-            forwardBasis.y = 0.0f;
-            rightBasis.y = 0.0f;
-            
-            forwardBasis.Normalize();
-            rightBasis.Normalize();
-
-            desiredMoveDirection = (forwardBasis * inputVector.y) + (rightBasis * inputVector.x);
-
-            if (desiredMoveDirection.sqrMagnitude > 0.0f)
-            {
-                lastNonZeroDesiredMoveDirection = desiredMoveDirection;
-            }
-
-            bool shouldResetGravity = isGrounded && (gravityVector.y < 0.0f);
+            bool shouldResetGravity = characterState.isGrounded && (gravityVector.y < 0.0f);
             
             if (shouldResetGravity)
             {
@@ -108,8 +171,46 @@ namespace Game
             }
             
             gravityVector.y = (gravity * Time.deltaTime);
+
+            Vector3 currentMoveDirection = desiredMoveDirection;
+            float currentMoveSpeed = moveSpeed;
+            float currentMoveSpeedMultiplier = 1.0f;
             
-            Vector3 velocity = desiredMoveDirection * (Time.deltaTime * moveSpeed);
+            bool shouldUseKnockback = (Time.time <= knockbackTimer);
+            
+            if (shouldUseKnockback)
+            {
+                currentMoveDirection = currentKnockbackSetting.knockbackDirection;
+                currentMoveSpeed = currentKnockbackSetting.knockbackSpeed;
+                currentMoveSpeedMultiplier = currentKnockbackSetting.knockbackSpeedMultipiler;
+            }
+            else if (characterState.isMoveable)
+            {
+                Vector3 forwardBasis = mainCamera.transform.forward;
+                Vector3 rightBasis = mainCamera.transform.right;
+
+                forwardBasis.y = 0.0f;
+                rightBasis.y = 0.0f;
+                
+                forwardBasis.Normalize();
+                rightBasis.Normalize();
+
+                desiredMoveDirection = (forwardBasis * inputVector.y) + (rightBasis * inputVector.x);
+
+                if (desiredMoveDirection.sqrMagnitude > 0.0f)
+                {
+                    lastNonZeroDesiredMoveDirection = desiredMoveDirection;
+                }
+
+                currentMoveDirection = desiredMoveDirection;
+            }
+            else
+            {
+                desiredMoveDirection = Vector3.zero;
+                currentMoveDirection = Vector3.zero;
+            }
+            
+            Vector3 velocity = currentMoveDirection * (Time.deltaTime * currentMoveSpeed * currentMoveSpeedMultiplier);
             velocity.y = gravityVector.y;
             
             characterController.Move(velocity);
@@ -119,6 +220,25 @@ namespace Game
         {
             Quaternion lookDirection = Quaternion.LookRotation(lastNonZeroDesiredMoveDirection);
             transform.rotation = Quaternion.Slerp (transform.rotation, lookDirection, DESIRED_ROTATION_SPEED);
+        }
+
+        public void Knockback(Vector3 knockbackDirection, KnockBackType knockbackType)
+        {
+            currentKnockBackType = knockbackType;
+            currentKnockbackSetting = knockBackSettings.FirstOrDefault(x => x.knockBackType == knockbackType);
+            currentKnockbackSetting.knockbackDirection = knockbackDirection;
+            knockbackTimer = (Time.time + currentKnockbackSetting.knockbackDuration);
+        }
+
+        // TODO
+        public void Dash(Vector3 dashDirection)
+        {
+            if (!CanDash)
+            {
+                return;
+            }
+            
+            // TODO
         }
     }
 }
